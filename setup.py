db9c154bc7272affb7b2c3afe9c0c355664499b8
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2025-06-02 22:45:08 +0000

    docs: Updated setup file with author details

--- setup.py
+++ setup.py
@@ -11,8 +11,17 @@ setup(
         ]
     },
     author="Abdou-Raouf ATARMLA",
+    author_email="achilleatarmla@gmail.com",
     description="A transpileur for Python in native languages",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fless-lab/multilang-python",
     license="MIT",
     python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     include_package_data=True,
 )
